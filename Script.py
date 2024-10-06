b590444c96e0642e9991db91ed6562242dd2ce17
maan12305 <167197439+maan12305@users.noreply.github.com>
2024-10-06 17:10:57 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -115,7 +115,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/maam_movies_group">PROFESSOR</a>」</b>"""
+    FILE_CAPTION = """**⍟ File Name🔻</b>\n\n{file_name}</a></b>\n\n<b>**⍟  File Size\n\n  🔻[ {file_size} ]</a></b>**"""
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
