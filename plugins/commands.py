12d893c9154afe1f7fb86a0a5365146517f94834
maan12305 <167197439+maan12305@users.noreply.github.com>
2024-10-12 09:10:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -354,8 +354,7 @@ async def start(client:Client, message):
                 file_caption=file.caption
             )
             btn = [[
-                InlineKeyboardButton("⚡ 𝙁𝙖𝙨𝙩 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙 / 𝙒𝙖𝙩𝙘𝙝 𝙊𝙣𝙡𝙞𝙣𝙚 ⚡", callback_data=f'stream#{file.file_id}')
-            ],[
+                InlineKeyboardButton("⚡ 𝙁𝙖𝙨𝙩 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙 / 𝙒𝙖𝙩𝙘𝙝 𝙊𝙣𝙡𝙞𝙣𝙚 ⚡", callback_data=f'stream#{file.file_id}')‚
                 InlineKeyboardButton("𝗠𝗼𝘃𝗶𝗲 𝗨𝗽𝗱𝗮𝘁𝗲", url=f'https://t.me/movie_update_channel0')
             ]]
             toDel = await client.send_cached_media(
@@ -396,8 +395,7 @@ async def start(client:Client, message):
         file_caption=files.caption
     )
     btn = [[
-        InlineKeyboardButton("⚡ 𝙁𝙖𝙨𝙩 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙 / 𝙒𝙖𝙩𝙘𝙝 𝙊𝙣𝙡𝙞𝙣𝙚 ⚡", callback_data=f'stream#{file.file_id}')
-    ],[
+        InlineKeyboardButton("⚡ 𝙁𝙖𝙨𝙩 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙 / 𝙒𝙖𝙩𝙘𝙝 𝙊𝙣𝙡𝙞𝙣𝙚 ⚡", callback_data=f'stream#{file.file_id}')‚
         InlineKeyboardButton("𝗠𝗼𝘃𝗶𝗲 𝗨𝗽𝗱𝗮𝘁𝗲", url=f'https://t.me/movie_update_channel0')
     ]]
     toDel=await client.send_cached_media(
