304a2a6b82eb323018110480ad9ce0ea3427c2da
maan12305 <167197439+maan12305@users.noreply.github.com>
2024-10-14 12:03:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -356,8 +356,7 @@ async def start(client:Client, message):
             btn = [[
                 InlineKeyboardButton("⚡ Fᴀꜱᴛ Dᴏᴡɴʟᴏᴀᴅ / Wᴀᴛᴄʜ Oɴʟɪɴᴇ ⚡", callback_data=f'stream#{file.file_id}')
             ],[ 
-                InlineKeyboardButton("Mᴏᴠɪᴇ Uᴘᴅᴀᴛᴇ ✪", url=f'https://t.me/movie_update_channel0'),
-                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ ✧", url=f'https://t.me/Im_professor_22')
+                InlineKeyboardButton("Mᴏᴠɪᴇ Uᴘᴅᴀᴛᴇ 📰", url=f'https://t.me/movie_update_channel0')
             ]]
             toDel = await client.send_cached_media(
                 chat_id=message.from_user.id,
@@ -399,8 +398,7 @@ async def start(client:Client, message):
     btn = [[
         InlineKeyboardButton("⚡ Fᴀꜱᴛ Dᴏᴡɴʟᴏᴀᴅ / Wᴀᴛᴄʜ Oɴʟɪɴᴇ ⚡", callback_data=f'stream#{file_id}')
     ],[
-        InlineKeyboardButton(" Mᴏᴠɪᴇ Uᴘᴅᴀᴛᴇ ✪", url=f'https://t.me/movie_update_channel0'),
-        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ ✧", url=f'https://t.me/Im_professor_22')
+        InlineKeyboardButton(" Mᴏᴠɪᴇ Uᴘᴅᴀᴛᴇ 📰", url=f'https://t.me/movie_update_channel0')
     ]]
     toDel=await client.send_cached_media(
         chat_id=message.from_user.id,
