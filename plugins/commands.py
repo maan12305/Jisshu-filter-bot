d465fd140e62c86cd08e996d568c51bc553b4655
maan12305 <167197439+maan12305@users.noreply.github.com>
2024-10-14 09:39:57 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -194,7 +194,7 @@ async def start(client:Client, message):
                             InlineKeyboardButton('Mᴏᴠɪᴇ Gʀᴏᴜᴘ ⚡', url="https://t.me/maam_movies_backup")
                         ]] 
                         reply_markup = InlineKeyboardMarkup(buttons)
-                        m=await message.reply_sticker("CAACAgQAAxkBAAEn9_ZmGp1uf1a38UrDhitnjOOqL1oG3gAC9hAAAlC74FPEm2DxqNeOmB4E") 
+                        m=await message.reply_sticker("CAACAgQAAxUAAWcMmCopsZU5QCTv38Plv7ST_-dkAAK5FgACx-tYUA6rJwhT6ONGNgQ") 
                         await asyncio.sleep(1)
                         await m.delete()
                         await message.reply_photo(photo=random.choice(START_IMG), caption=script.START_TXT.format(message.from_user.mention, get_status(), message.from_user.id),
@@ -223,7 +223,7 @@ async def start(client:Client, message):
                             InlineKeyboardButton('Mᴏᴠɪᴇ Gʀᴏᴜᴘ ⚡', url="https://t.me/maam_movies_backup")
                         ]] 
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgQAAxkBAAEn9_ZmGp1uf1a38UrDhitnjOOqL1oG3gAC9hAAAlC74FPEm2DxqNeOmB4E") 
+        m=await message.reply_sticker("CAACAgQAAxUAAWcMmCopsZU5QCTv38Plv7ST_-dkAAK5FgACx-tYUA6rJwhT6ONGNgQ") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(photo=random.choice(START_IMG), caption=script.START_TXT.format(message.from_user.mention, get_status(), message.from_user.id),
