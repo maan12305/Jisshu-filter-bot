7b2459cb8de0f98a2afcbc5865cdd38b9c68f7dd
maan12305 <167197439+maan12305@users.noreply.github.com>
2024-10-15 18:46:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -538,7 +538,7 @@ async def quality_search(client: Client, query: CallbackQuery):
    # btn.insert(0,[
 	#InlineKeyboardButton("📥 𝗦𝗲𝗻𝗱 𝗔𝗹𝗹 𝗙𝗶𝗹𝗲𝘀 📥", callback_data=batch_link),
      #   ])
-    btn.insert(1, [
+   # btn.insert(1, [
       #  InlineKeyboardButton("ǫᴜᴀʟɪᴛʏ", callback_data=f"qualities#{key}#{offset}#{req}"),
 	#InlineKeyboardButton("ꜱᴇᴀꜱᴏɴ", callback_data=f"seasons#{key}#{offset}#{req}"),
       #  InlineKeyboardButton("ʟᴀɴɢᴜᴀɢᴇ", callback_data=f"languages#{key}#{offset}#{req}"),
